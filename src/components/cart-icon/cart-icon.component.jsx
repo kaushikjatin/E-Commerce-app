f462aba7a2fe226c8135c9ea8754e45be24e3b23
Jatin kaushik <jatinkaushik@Jatins-MacBook-Air.local>
2021-03-27 07:32:01 +0530

    Completed ADD TO CART Functionality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import {ReactComponent as ShoppinIcon} from '../../assets/shopping-bag.svg'
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
 import {connect} from 'react-redux'
 import './cart-icon.styles.scss'
 
-const cartIcon = ({toggleCartHidden}) =>(
+const cartIcon = ({toggleCartHidden,totalCount}) =>(
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppinIcon className='shopping-icon'></ShoppinIcon>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{totalCount}</span>
     </div>
 );
 
+
+const mapstatetoprops = (state) =>({
+    totalCount : selectCartItemsCount(state)
+})
 const mapdispatchtoprops= (dispatch)=>({
             toggleCartHidden:()=>dispatch(toggleCartHidden())
 })
-export default connect(null,mapdispatchtoprops)(cartIcon);
+export default connect(mapstatetoprops,mapdispatchtoprops)(cartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,14 @@
+import {createSelector} from 'reselect'
+
+const selectcart = (state) => state.cart
+
+export const selectCartItems = createSelector(
+    [selectcart],
+    (cart) => cart.cartItems
+)
+
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    (cartItems) => cartItems.reduce((count,item)=> count + item.quantity , 0)
+)

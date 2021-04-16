5eed2f5f925e0f15177ea0772b6bbeb749424afb
Jatin kaushik <jatinkaushik@Jatins-MacBook-Air.local>
2021-04-16 06:02:41 +0530

    Alpha Version is Ready(Frontend Completed)

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,8 @@ import {persistStore} from 'redux-persist'
 import logger from 'redux-logger'
 import PersistReducer from './root-reducer'
 
-const middlewares= [logger];
+const middlewares= [];
+if(process.env.NODE_ENV=='development')
+    middlewares.push(logger);
 export const store = createStore(PersistReducer,applyMiddleware(...middlewares));
 export const persistor= persistStore(store);

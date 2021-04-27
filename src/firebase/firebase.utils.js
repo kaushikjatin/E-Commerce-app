7918116065abde0d9ddda29d5b651bda6670daf8
Jatin kaushik <jatinkaushik@Jatins-MacBook-Air.local>
2021-04-27 07:11:33 +0530

    Shifted database to remote

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,9 @@
 import firebase from 'firebase/app'; //this base import is useful because all the auth and firesotre are automatially connected to the firebase keyword which we impoted
 import 'firebase/firestore';
 import 'firebase/auth';
+import {firebase_config} from '../important_data'
 
-const config={
-    apiKey: "AIzaSyC9qWZrYXZJdYdJkBQ3Lgpv3SA5IoQS7cc",
-    authDomain: "crwn-db-9f540.firebaseapp.com",
-    projectId: "crwn-db-9f540",
-    storageBucket: "crwn-db-9f540.appspot.com",
-    messagingSenderId: "3292108503",
-    appId: "1:3292108503:web:e1100ec97b02ecf5608292",
-    measurementId: "G-2GW589V8L4"
-  };
+const config=firebase_config
 
   firebase.initializeApp(config);
 

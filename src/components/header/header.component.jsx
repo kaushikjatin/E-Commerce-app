db9a5c14573080b7a83c42a3c34a1e1c8eed2964
Jatin kaushik <jatinkaushik@Jatins-MacBook-Air.local>
2021-05-16 07:26:30 +0530

    Made it responsive

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Link} from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils.js';
@@ -7,23 +7,29 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import {selectCartHidden} from '../../redux/cart/cart.selectors'
 import {selectCurrentUser} from '../../redux/user/user.selectors'
-import {HeaderContainer , OptionDiv,OptionLink,OptionsContainer,LogoContainer} from './header.styles.jsx'
+import {HeaderContainer , OptionDiv,OptionLink,OptionsContainer,LogoContainer,AiFillDatabase1,Right_Side_Container} from './header.styles.jsx'
 
 const Header=({currentUser,hidden})=>{
+    const [hiddenMenuBar,setHiddenMenuBar]=useState(false);
     return(
         <HeaderContainer>
             <LogoContainer to='/'>
                 <Logo/>
             </LogoContainer>
-            <OptionsContainer>
-                <OptionLink to='/shop'>Shop</OptionLink>
-                <OptionLink to='/shop'>Contact</OptionLink>
-                {currentUser ?(
-                        <OptionDiv onClick={()=>auth.signOut()}>Sign Out</OptionDiv>
-                ):(<OptionLink to='/signin'>Sign In</OptionLink>)
-                }
-                <CartIcon></CartIcon>
-            </OptionsContainer>
+            {
+                hiddenMenuBar? null :( <OptionsContainer>
+                    <OptionLink to='/shop'>Shop</OptionLink>
+                    <OptionLink to='/shop'>Contact</OptionLink>
+                    {currentUser ?(
+                            <OptionDiv onClick={()=>auth.signOut()}>Sign Out</OptionDiv>
+                    ):(<OptionLink to='/signin'>Sign In</OptionLink>)
+                    }
+                </OptionsContainer>)
+            }
+            <Right_Side_Container>
+                    <CartIcon></CartIcon>
+                    <AiFillDatabase1 onClick={() =>setHiddenMenuBar(!hiddenMenuBar)}></AiFillDatabase1>
+            </Right_Side_Container>
             {
                 hidden? null : (<CartDropdown></CartDropdown>)
             }
@@ -34,7 +40,7 @@ const Header=({currentUser,hidden})=>{
 
 const mapStateToProps= (state)=>({   // mapStateToProps function will br called with argument as the state of the store...which is a big giant object
         currentUser:selectCurrentUser(state),
-        hidden:selectCartHidden(state)
+        hidden:selectCartHidden(state),
     }) // this function will return an object which will be given to our Header component as props automatically.
 
 export default connect(mapStateToProps)(Header);
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,34 +1,81 @@
 import styled,{css} from 'styled-components'
 import {Link} from 'react-router-dom'
+import {AiFillDatabase,AiOutlineBars} from 'react-icons/ai' 
 
 const OptionContainerStyles = css`
 padding:10px 15px;
 cursor:pointer`
 
 export const HeaderContainer = styled.div`
-height: 70px;
+min-height: 70px;
 width: 100%;
 display: flex;
 flex-direction: row;
-justify-content: space-between;
-margin-bottom: 25px;`
+margin-bottom: 25px;
+position:relative;
+@media (max-width:660px)
+{
+    max-height:300px;
+    justify-content:space-between
+}`
+
 
 export const LogoContainer =styled(Link)`
 height: 100%;
-width: 90px;
+width: 15%;
 padding: 20px;
+box-sizing:border-box;
 padding-top:10px;
 background:white;
+display:flex;
+align-items:center;
+justify-content:center;
 overflow:hidden;
 border-radius:50%;`
 
+
 export const OptionsContainer = styled.div`
-width: 30%;
+width: 70%;
 height: 100%;
 display: flex;
 align-items: center;
-justify-content: space-between;
-font-size : 20px`
+justify-content: flex-end;
+font-size : 20px;
+
+@media (max-width:660px)
+{
+    height:300px;
+    width:40%;
+    border-left:5px solid white;
+    position:absolute;
+    right:0px;
+    flex-direction:column;
+    background:black;
+    z-index:10;
+}
+`
+
+export const Right_Side_Container=styled.div`
+    width: 15%;
+    min-height: 100%;
+    display:flex;
+    justify-content:space-between;
+    box-sizing:border-box;
+    align-items:center;
+`
+
+
+export const AiFillDatabase1=styled(AiFillDatabase)`
+width:40%;
+height:60px;
+z-index:50;
+display:none;
+@media (max-width:660px)
+{
+    display:inline;
+}
+`
+
 
 export const OptionDiv =styled.div`
 ${OptionsContainer}`
@@ -36,5 +83,19 @@ ${OptionsContainer}`
 export const OptionLink =styled(Link)`
 ${OptionsContainer};
 color: white;
-text-decoration: none;`
+text-decoration: none;
+margin:25px;
+
+@media (max-width:660px)
+{
+    margin:0px;
+    width:100%;
+    height:30%;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    border-bottom:5px solid white;
+}
+`
+
 

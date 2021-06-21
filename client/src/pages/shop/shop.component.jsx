fa4c8eb6c4d322cf2411c335e9baa42f8d5f2219
Jatin kaushik <jatinkaushik@Jatins-MacBook-Air.local>
2021-06-21 13:40:47 +0530

    Shifted from class component to functional componenet

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component,useEffect } from 'react';
 import {Route} from 'react-router-dom'
 import {connect} from 'react-redux';
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 import CollectionOverviewContainer from '../../components/collection-overview/collection-overview.container'
 import CollectionPageContainer from '../collection/collection.container'
 
-class ShopPage extends React.Component
-{
-    componentDidMount(){
-        const {fetchCollectionsStart} = this.props;
-        fetchCollectionsStart();
-    }
+const ShopPage =({fetchCollectionsStart,match})=>{
+    useEffect(()=>{
+        fetchCollectionsStart()
+    },[fetchCollectionsStart])
 
-    render(){
-        const {match}=this.props;
         return(
             <div className='shop-page'>
                 <Route exact path={`${match.path}`} component={CollectionOverviewContainer}></Route>
@@ -22,7 +18,6 @@ class ShopPage extends React.Component
                 into the function as arguments....and hence we will then pass the props into the componenets */}
             </div>
         )
-    }
 }
 
 const mapDispatchToProps = dispatch =>({
